a3f423fa4d2662fe68f25dec5daaabb18b48478c
OSSportal <ossportal2017@gmail.com>
2017-11-29 17:07:31 +0900

    add new skin

---
+++ components/OSSNoDF/OSSNoDFSkin.php
@@ -0,0 +1,17 @@
+<?php
+namespace Xpressengine\Plugins\OSSBoardSkins\Components\OSSNoDF;
+
+use Xpressengine\Plugins\OSSBoardSkins\Components\OSS\OSSSkin;
+use XeSkin;
+use Event;
+use Auth;
+use Gate;
+
+class OSSNoDFSkin extends OSSSkin
+{
+    protected static $path = 'oss_board_skins/components/OSSNoDFSkin';
+
+    public static function boot()
+    {
+    }
+}
