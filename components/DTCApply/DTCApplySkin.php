34cfa050487f292e9965aa549d597e833cb6359c
OSSportal <ossportal2017@gmail.com>
2019-08-02 13:25:15 +0900

    컨트리뷰톤 참가신청 스킨 추가

---
+++ components/DTCApply/DTCApplySkin.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Xpressengine\Plugins\OSSBoardSkins\Components\DTCApply;
+
+use Xpressengine\Plugins\OSSBoardSkins\Components\OSS\OSSSkin;
+
+class DTCApplySkin extends OSSSkin
+{
+    protected static $path = 'oss_board_skins/components/DTCApply';
+}
